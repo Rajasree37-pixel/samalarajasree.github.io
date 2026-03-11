6fd474f16f3896f9b0bc883d68d2117e088b4c01
Samala Raja Sree <rajasreesamala2004@gmail.com>
2026-03-11 16:58:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -64,3 +64,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', function() {
+    // This toggles the animation for the hamburger bars
+    menu.classList.toggle('is-active'); 
+    // This actually opens/closes the menu overlay
+    menuLinks.classList.toggle('active'); 
+});
+
+// Close mobile menu when a link is clicked
+document.querySelectorAll('.nav-item').forEach(n => n.addEventListener('click', () => {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+}));
